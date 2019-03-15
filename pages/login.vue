34cfb3c0ea908cf2eaf4442c27154baf8b0637b1
wangcan <wangcan@021.com>
2019-03-15 15:44:45 +0800

    搭建项目结构

---
+++ pages/login.vue
@@ -0,0 +1,26 @@
+<template>
+	<view>
+		<view class="title">{{title}}</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: '登录页'
+			};
+		},
+		onLoad() {
+			console.log('请登录')
+		}
+	}
+</script>
+
+<style>
+	.title {
+		font-size: 24px;
+		color: red;
+		text-align: center;
+		}
+</style>
